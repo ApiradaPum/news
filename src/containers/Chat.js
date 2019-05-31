5a27a4d0fb6388a1a95efa29fe8772c54ddec662
Mint-Macnmotion <apirada@macnmotion.com>
2019-05-31 16:29:50 +0700

    Add header

---
+++ src/containers/Chat.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import picEmpty from '././'
+
+class Chat extends Component {
+	constructor() {
+	  super()
+  
+	  	this.state = {
+			colors: ['darksalmon', 'deepskyblue', 'darkgreen', 'cornflowerblue', 'brown', 'coral', 'blueviolet','red', 'blue'],
+			input: '',
+			username: '',
+	  	}
+	}
+	
+  
+	render() {
+		return (
+			<div>
+				<div className="blog-news">
+					<img className="img-responsive" src=""
+				</div>
+			</div>
+				
+			)
+		}
+	}
+  
+  
+  export default Chat
---
+++ src/containers/Header.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+	constructor() {
+	  super()
+	  	this.state = {
+			input: '',
+			username: '',
+	  	}
+	}
+	
+  
+	render() {
+		return (
+			<div className="nav-bar bg-dark-mint">
+                sdasdasd
+
+			</div>
+			)
+		}
+	}
+  
+  
+  export default Header
