713874f3398031c6df5cf8e3d4a584f648299cea
ApiradaPumpan <apirada@macnmotion.com>
2019-06-01 01:38:22 +0700

    update

--- src/containers/Chat.js
+++ src/containers/Chat.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import picEmpty from '././'
+import {Container, Row, Col} from 'react-bootstrap';
 
 class Chat extends Component {
 	constructor() {
@@ -16,9 +16,27 @@ class Chat extends Component {
 	render() {
 		return (
 			<div>
-				<div className="blog-news">
-					<img className="img-responsive" src=""
-				</div>
+				
+				<Container>
+					<Row>
+						<Col sm={4} xs={12}>
+							<div className="blog-news">
+								<h2>Title</h2>
+								<img className="img-responsive" src={'/img/empty.png'} />
+								<p>Lorem Ipsum is simply dummy text of the printing and
+								 typesetting industry. Lorem Ipsum has been the industry's standard 
+								 dummy text ever since the 1500s, when an unknown printer took a 
+								 galley of type and scrambled it to make a type specimen book. 
+								 It has survived not only five centuries, but also the leap into 
+								 electronic typesetting, remaining essentially unchanged. It was 
+								 popularised in the 1960s with the release of Letraset sheets 
+								 containing Lorem Ipsum passages, and more recently with desktop 
+								 publishing software like Aldus PageMaker including versions of 
+								 Lorem Ipsum. </p>
+							</div>
+						</Col>
+					</Row>
+				</Container>
 			</div>
 				
 			)
