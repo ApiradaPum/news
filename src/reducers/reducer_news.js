a4a782af1bb0a30c0139c8f44edd3622031b8bcb
Apirada <mmitnn@gmail.com>
2019-06-02 18:07:07 +0700

    Update get data

--- src/reducers/reducer_news.js
+++ src/reducers/reducer_news.js
@@ -1,14 +1,13 @@
 import _ from "lodash";
 import { GET_NEWS, FETCH_NEWS, SEARCH_NEWS } from "../actions";
-import news from '../json/news';
 import news100 from '../json/news100';
 
 export default function(state = {}, action){
 	switch(action.type){
 		case FETCH_NEWS:
-			return news100;
+			return _.slice(news100, 0, 15);
 		case GET_NEWS:
-			return _.concat(state, action.payload);
+			return _.concat(state, _.slice(news100, action.payload.length, action.payload.length+15));
 		case SEARCH_NEWS:
 			return _.find(state, action.payload);
 		default:
